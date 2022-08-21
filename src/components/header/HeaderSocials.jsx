04f688f4429d80ae05ce83bc25225e02c077b085
jetaypedev <jetaypedev@gmail.com>
2022-08-21 15:05:29 -0500

    actualizacion 3

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { BsLinkedin} from 'react-icons/bs'
-import { BsGithub} from 'react-icons/bs'
-import { BsFacebook} from 'react-icons/bs'
+import { BsLinkedin } from 'react-icons/bs'
+import { BsGithub } from 'react-icons/bs'
+import { BsFacebook } from 'react-icons/bs'
 
 export const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-      <a href="www.linkedin.com/in/jetaypedev" target='_blank'><BsLinkedin /></a>
+      <a href="https://www.linkedin.com/in/jetaypedev" target='_blank'><BsLinkedin /></a>
       <a href="https://github.com/jetaype21" target='_blank'><BsGithub /></a>
       <a href="https://github.com/jetaype21" target='_blank'><BsFacebook /></a>
     </div>
